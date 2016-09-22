a4db6737ff835d280b226ca1267f447675e82dee
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-09-22 16:05:52 +0300

    Added mess detector parser

--- src/MessDetector.php
+++ src/MessDetector.php
@@ -12,6 +12,12 @@ namespace samsonframework\bitbucket;
  */
 class MessDetector
 {
+    /** XML File path field */
+    const FILEPATH = 'file';
+
+    /** XML File line number  */
+    const LINENUMBER = 'beginline';
+
     /** @var array XML PHP mess detector data */
     protected $xmlData = [];
 
@@ -30,6 +36,11 @@ class MessDetector
         return $out;
     }
 
+    /**
+     * MessDetector constructor.
+     *
+     * @param string $xmlPath Path to mess detector xml file
+     */
     public function __construct(string $xmlPath)
     {
         // Read XML and convert to array
@@ -45,11 +56,10 @@ class MessDetector
         /** @var array $violations Collection of violations grouped by files and lines */
         $violations = [];
 
-        $pointer = &$violations;
         foreach ($this->xmlData->file as $file) {
-            $pointer = &$violations[(string)$file['name']];
+            $pointer = &$violations[(string)$file[self::FILEPATH]];
             foreach ($file->violation as $violation) {
-                $pointer[(string)$violation['beginline']] = trim((string)$violation);
+                $pointer[(string)$violation[self::LINENUMBER]] = trim((string)$violation);
             }
 
         }
